3e98250bed3a05d2eebe19dc4f091f9dd226ad39
Isaac Druin <isaac.druin@gmail.com>
2023-11-26 16:48:32 -0600

    started to implement routing...

---
+++ pages/about/index.tsx
@@ -0,0 +1,16 @@
+const AboutUs = () => {
+	return (
+		<div className='flex flex-col'>
+			<p>1</p>
+			<p>1</p>
+			<p>1</p>
+			<p>1</p>
+			<p>1</p>
+			<p>1</p>
+		</div>
+	);
+};
+
+export default function About() {
+	return <AboutUs />;
+}
---
+++ pages/goals/index.tsx
@@ -0,0 +1,16 @@
+const OurGoals = () => {
+	return (
+		<div className='flex flex-col'>
+			<p>1</p>
+			<p>1</p>
+			<p>1</p>
+			<p>1</p>
+			<p>1</p>
+			<p>1</p>
+		</div>
+	);
+};
+
+export default function About() {
+	return <OurGoals />;
+}
