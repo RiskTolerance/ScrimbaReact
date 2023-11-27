6831ba97aa24349e507f1acd8dfc2fdf91ade339
Isaac Druin <isaac.druin@gmail.com>
2023-11-27 15:49:06 -0600

    Added a navbar along with links to other pages.

---
+++ pages/projects/index.tsx
@@ -0,0 +1,17 @@
+const OurGoals = () => {
+	return (
+		<>
+			<h1>This is a list of links to the projects</h1>
+			<ul>
+				<li>Project 1</li>
+				<li>Project 2</li>
+				<li>Project 3</li>
+				<li>Project 4</li>
+			</ul>
+		</>
+	);
+};
+
+export default function About() {
+	return <OurGoals />;
+}
